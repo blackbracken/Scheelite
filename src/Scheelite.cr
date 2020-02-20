cc24618d879a742d454516185b8d601bc1e7fbb5
blackbracken <me@bracken.black>
2020-02-20 22:54:40 +0900

    send message with result to ping

--- src/Scheelite.cr
+++ src/Scheelite.cr
@@ -1,6 +1,27 @@
-# TODO: Write documentation for `Scheelite`
-module Scheelite
-  VERSION = "0.1.0"
+require "file"
+require "yaml"
+require "http/client"
+require "pinger"
 
-  # TODO: Put your code here
+CONFIG_PATH = "./config.yaml"
+
+if !File.exists?(CONFIG_PATH)
+  config = File.open CONFIG_PATH, "w"
+  config.puts <<-DEFAULT_CONFIG
+    Slack:
+      WebhookURL: "https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    Pinged:
+      - "xxx.xxx.xxx.xxx"
+      - "google.com"
+    DEFAULT_CONFIG
+  config.close
+end
+
+File.open(CONFIG_PATH) do |file|
+  config = YAML.parse file
+  url = config["Slack"]["WebhookURL"].as_s
+  pinger = Pinger.new("google.com", count: 4)
+  response = HTTP::Client.post url, body: "{ \"text\": \"Hello, yoneyan #{pinger.ping}\" }"
+  
+  puts response.body
 end
